d5234669c0550fe0eb7650e8f46bdb5a7d08c3ee
ploy <chonnikan.t@kkumail.com>
2018-07-09 15:00:16 +0700

    [Added]: เพิ่มส่วนของการดึงข้อมูลจาก database และ แก้ไขข้อมูลตรง connect database

--- src/main.go
+++ src/main.go
@@ -7,11 +7,38 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+	ID              int
+	CitizenID       string
+	Firstname       string
+	Lastname        string
+	BirthYear       int
+	FirstnameFather string
+	LastnameFather  string
+	FirstnameMother string
+	LastnameMother  string
+	SoldierID       int
+	AddressID       int
+}
+
 func main() {
-	_, err := sql.Open("mysql", "root:@/sckshuhari/tcp(127.0.0.1:3306)/testsck")
+	db, err := sql.Open("mysql", "root:sckshuhari@tcp(127.0.0.1:3306)/testsck")
+
 	if err != nil {
 		fmt.Println("connect fail")
 
 	}
 	fmt.Println("connect success")
+	defer db.Close()
+	results, _ := db.Query("SELECT * FROM user")
+
+	for results.Next() {
+		var user User
+		err := results.Scan(&user.ID, &user.Firstname, &user.Lastname, &user.CitizenID, &user.BirthYear, &user.FirstnameFather, &user.LastnameFather, &user.FirstnameMother, &user.LastnameMother, &user.SoldierID, &user.AddressID)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(user)
+	}
+
 }
