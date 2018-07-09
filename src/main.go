861b6ae91197b24a2044ddbde7b0adf987182bd7
ploy <chonnikan.t@kkumail.com>
2018-07-09 15:18:55 +0700

    [Added]: เพิ่มฟังก์ชั่น read มาเพื่อแยกการทำงานออกจากกัน และส่งค่ากลับไปเป็ยอาร์เรย์

--- src/main.go
+++ src/main.go
@@ -23,22 +23,26 @@ type User struct {
 
 func main() {
 	db, err := sql.Open("mysql", "root:sckshuhari@tcp(127.0.0.1:3306)/testsck")
-
+	defer db.Close()
 	if err != nil {
 		fmt.Println("connect fail")
 
 	}
 	fmt.Println("connect success")
-	defer db.Close()
-	results, _ := db.Query("SELECT * FROM user")
+	fmt.Println(read(db))
 
+}
+
+func read(db *sql.DB) []User {
+	results, _ := db.Query("SELECT * FROM user")
+	var userList []User
 	for results.Next() {
 		var user User
 		err := results.Scan(&user.ID, &user.Firstname, &user.Lastname, &user.CitizenID, &user.BirthYear, &user.FirstnameFather, &user.LastnameFather, &user.FirstnameMother, &user.LastnameMother, &user.SoldierID, &user.AddressID)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(user)
+		userList = append(userList, user)
 	}
-
+	return userList
 }
